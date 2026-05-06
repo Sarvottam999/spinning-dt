cf708912e22cff660670b3c21dd6e1077fd3660c
sarvottam_sharma <sarvottamsharma999@gmail.com>
2026-05-07 00:59:51 +0530

    added acid plant :  FIXED LINES

--- app/features/AcidPlantAnalysis.tsx
+++ app/features/AcidPlantAnalysis.tsx
@@ -3,13 +3,36 @@
 import ExcelUploader from "../Breakdowntable";
 
 const acidPlantLineGroups: Record<string, string[]> = {
-  "AP # 1": ["P3-ACD-AP1", "MP-ANC-ACD-ACP1", "MP-ANC-ACD-AC01"],
+  "AP # 1": [
+    "P3-ACD-AP1",
+    "MP-ANC-ACD-ACP1",
+    "MP-ANC-ACD-AC01",
+  ],
+
   "AP # 2": [
     "P3-ACD-AP2",
     "MP-ACD-AC2",
     "MP-CSA-AP2",
     "MP-ANC-ACD-ACP2",
     "MP-ANC-ACD-AC02",
+  ],
+
+  "AP # 3": [
+    "P3-ACD-AP3",
+    "MP-ANC-ACD-ACP3",
+    "MP-ANC-ACD-AC03",
+  ],
+
+  "AP # 4": [
+    "P3-ACD-AP4",
+  ],
+
+  AP: [
+    "MP-ACD",
+  ],
+
+  WSA2: [
+    "MP-ANC-WSA-WSA2",
   ],
 };
 
